14fcf19d913d0e5480d85cfc8ae8b63200e74358
Praneesh <parshipraneesh8@gmail.com>
2024-05-24 22:59:02 +0530

    new header (progress indicator)

--- src/Root.jsx
+++ src/Root.jsx
@@ -4,12 +4,19 @@ import Header from "./components/Header";
 import PropTypes from 'prop-types';
 
 export default function Root(props) {
-  
+  var bodyElement = document.getElementsByClassName('head')[0]; 
+  var listElement = document.getElementsByClassName('list')[0];
+  var listHeight = listElement ? listElement.offsetHeight : 0;
+  var bodyHeight = bodyElement ? bodyElement.offsetHeight : 0;
   return (
     <>
       <div className="body">
         <Header route={props.route} setRoute={props.setRoute} />
+        <div className="outlet" style={{
+          paddingTop: bodyHeight+listHeight
+        }}>
         <Outlet />
+        </div>
         <Footer />
       </div>
     </>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,50 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import PropTypes from "prop-types";
+import { motion, useScroll, useMotionValueEvent } from "framer-motion";
 export default function Header(props) {
   let location = useLocation();
+  const { scrollYProgress } = useScroll();
+  const [lenProgress, setLenProgress] = useState(1);
+  const [progressLen, setProgressLen] = useState(0);
+  const [lastestProgress, setLatestProgress] = useState(0);
+
   useEffect(() => {
     handleRouteChange(location.pathname);
   }, [location.pathname]);
+
+  // useEffect(() => {
+  //   var element = document.getElementsByClassName('header')[0];
+  //   var elementSpan = document.getElementsByClassName('progress_span')[1];
+  //   var elementLogo = document.getElementsByClassName('logo')[0];
+
+  //   if (element && elementSpan && elementLogo) {
+  //     var width = element.offsetWidth;
+  //     var widthSpan = elementSpan.offsetWidth;
+  //     var widthLogo = elementLogo.offsetWidth;
+  //     var len = (Math.round((width-widthLogo)/widthSpan));
+  //     setLenProgress(len);
+  //   }
+  // }, []);
+
+  const widthProgressFinder = () => {
+    var element = document.getElementsByClassName("header")[0];
+    var width = element.offsetWidth;
+    var elementSpan = document.getElementsByClassName("progress_span")[0];
+    var widthSpan = elementSpan.offsetWidth;
+    var elementLogo = document.getElementsByClassName("logo")[0];
+    var widthLogo = elementLogo.offsetWidth;
+    var len = Math.round((width - widthLogo) / widthSpan);
+    setLenProgress(len);
+    console.log(len);
+  };
+
+  useMotionValueEvent(scrollYProgress, "change", (latest) => {
+    widthProgressFinder();
+    setLatestProgress(latest);
+    setProgressLen(Math.round(lastestProgress * lenProgress));
+    console.log(lenProgress, progressLen);
+  });
 
   function handleRouteChange(pathname) {
     switch (pathname) {
@@ -50,25 +89,77 @@ export default function Header(props) {
       ...props.route,
       loc: newLoc,
     });
+    setTimeout(() => {
+      widthProgressFinder();
+      setProgressLen(Math.round(lastestProgress * lenProgress));
+    }, 100);
   }
 
   function handleMouseLeave() {
-    let originalLoc = props.route.loc.split(" cd ")[0];
+    let originalLoc = props.route.loc.split("cd")[0];
     props.setRoute({
       ...props.route,
       loc: originalLoc,
     });
+    setTimeout(() => {
+      widthProgressFinder();
+    }, 100);
   }
+  var bodyElement = document.getElementsByClassName("outlet")[0];
+  var bodyWidth = bodyElement ? bodyElement.offsetWidth : 0;
 
+  var logoElement = document.getElementsByClassName("header_space")[0];
+  var logoHeight = logoElement ? logoElement.offsetHeight : 0;
   return (
     <>
-      <div className="header">
+      <div
+        className="header"
+        style={{
+          width: bodyWidth,
+        }}
+      >
         <div className="head">
-          <div className={`repeat_${props.route.theme}`}>
-            <div id="loc" className={`logo ${props.route.theme}`}>
-              {props.route.loc}
-            </div>
+          <div id="loc" className={`header_space ${props.route.theme}`}>
+            <span className="logo">{props.route.loc}</span>
           </div>
+          <motion.div className="progress_cont">
+          <span
+                  className={`progress_span ${props.route.theme}_color`}
+                  style={{ fontSize: logoHeight }}
+                >
+                  {"["}
+                </span>
+            {Array(progressLen)
+              .fill()
+              .map((_, i) => (
+                <span
+                  className={`progress_span ${props.route.theme}_color`}
+                  key={i}
+                  style={{ fontSize: logoHeight }}
+                >
+                  #
+                </span>
+              ))}
+            {Array(
+              lenProgress - progressLen > 0 ? lenProgress - progressLen : 0
+            )
+              .fill()
+              .map((_, i) => (
+                <span
+                  className={`progress_span ${props.route.theme}_color`}
+                  key={i}
+                  style={{ fontSize: logoHeight }}
+                >
+                  .
+                </span>
+              ))}
+               <span
+                  className={`progress_span ${props.route.theme}_color`}
+                  style={{ fontSize: logoHeight }}
+                >
+                  {"]"}
+                </span>
+          </motion.div>
         </div>
         <ul className="list">
           <Link className="link" to="">
